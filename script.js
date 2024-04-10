9551ef649d922f1746ca38c6422ff13a12f15070
ritik chauhan <hritiksanhals@gmail.com>
2024-04-10 16:08:29 +0530

    add task and show task done

--- script.js
+++ script.js
@@ -17,7 +17,53 @@ const hideTaskbar = () =>{
     inpContainer.setAttribute("hidden", true);
     inpHead.removeAttribute("hidden", true);
 }
+
+// get data
+
+const getTodo = () =>{
+    let todos = localStorage.getItem("todos");
+    return todos ? JSON.parse(todos) : [];
+}
+
+
+// save data
+
+const addTask = (todos) =>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+}
+
+// show data
+const displayTodo = () =>{
+    let todoList = document.querySelector("#alltask");
+    let todos = getTodo();
+    todoList.innerHTML = "";
+
+    todos.forEach(element => {
+        let li = document.createElement("li");
+        li.textContent = element.text;
+        todoList.appendChild(li);
+        console.log(element)
+    });
+}
+
+
 inpBtn.addEventListener("click",()=>{
     hideTaskbar();
-    console.log(inpTask.value)
+    todoText = inpTask.value;
+    if(todoText){
+        let todos = getTodo();
+        addTask(todos);
+        let newTodo = {text: todoText};
+        todos.push(newTodo);
+        addTask(todos);
+        displayTodo();
+
+    }
+    else{
+        alert("please enter a todo");
+    }
+
+    inpTask.value = "";
+    
 })
+displayTodo();
